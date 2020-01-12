da4ee5e0338cb553ab432e1735de22769551a2f2
Pierre35557 <44743268+Pierre35557@users.noreply.github.com>
2020-01-12 16:37:16 +0200

    Add files via upload

---
+++ CoinCalculation/Helper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CoinCalculation
+{
+    public static class Helper
+    {
+        public static string[] Calculate(int[] coinDenominations, decimal cost, decimal amountPaid)
+        {
+            var result = new List<string>();
+            //Convert into cents, this way no funky divisions are made (I.E working with integers)
+            var amountOwed = (amountPaid - cost) * 100;
+
+            foreach (var denomination in coinDenominations)
+            {
+                while (denomination <= amountOwed)
+                {
+                    amountOwed -= denomination;
+                    result.Add(denomination.ToString());
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ CoinCalculation/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinCalculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "", cost = "", amountPaid = "";
+            bool validDenominations = false, validCost = false, validAmount = false;
+
+            Console.WriteLine("Please enter comma seperated denominations:");
+
+            input = Console.ReadLine();
+
+            while (validDenominations)
+            {
+                if (!string.IsNullOrEmpty(input) && input.Contains(",") && input.Split(',').All(c => c.All(char.IsDigit)))
+                    validDenominations = true;
+
+                Console.WriteLine("Please enter valid denominations:");
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine("Please enter vendor cost:");
+            cost = Console.ReadLine();
+
+            while (validCost)
+            {
+                if (cost.All(c => char.IsDigit(c)))
+                    validCost = true;
+
+                Console.WriteLine("Please enter valid cost:");
+                cost = Console.ReadLine();
+            }
+
+            Console.WriteLine("Please enter amount paid:");
+            amountPaid = Console.ReadLine();
+
+            while (validAmount)
+            {
+                if (amountPaid.All(c => char.IsDigit(c)))
+                    validAmount = true;
+
+                Console.WriteLine("Please enter valid amount paid:");
+                amountPaid = Console.ReadLine();
+            }
+
+            var denominations = input.Split(',').Select(int.Parse).OrderByDescending(c => c).ToArray();
+
+            var coins = Helper.Calculate(denominations, Convert.ToDecimal(cost), Convert.ToDecimal(amountPaid));
+
+            Console.WriteLine($"Coins Returned: {string.Join(",", coins)}");
+            Console.Read();
+        }
+    }
+}
